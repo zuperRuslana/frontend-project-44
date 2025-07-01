c88b87dac07527aa97f56b1d759fccb09919e5a6
Ruslana <lanasigalova@gmail.com>
2025-07-01 17:35:50 -0400

    improved logic calc.js

--- src/calc.js
+++ src/calc.js
@@ -2,26 +2,27 @@ import readlineSync from 'readline-sync';
 import getName from './cli.js';
 import { getRandomInt} from './even.js';
 
+
 function generateOperator(){
 const operators=['+','-','*'];
-let index= getRandomInt(0,2);
-let operator = operators[index];
-return operator;
+let index= getRandomInt(0,3);
+return operators[index]; 
 };
 
-function calculate(){
+export default function calculate(){
 const name = getName();
+let correctCount = 0;
+
+
+console.log('What is the result of the expression?');
+
+while (correctCount < 3) {
 let num1= getRandomInt(0,100);
 let num2= getRandomInt(0,100);
 let operator= generateOperator();
-console.log('What is the result of the expression?');
 console.log(`Question: ${num1} ${operator} ${num2}`);
-let answer;
-do{
-	 answer = Number(readlineSync.question('Your answer: '));
-}while(isNaN(answer) || !answer);
-
 let correctAnswer;
+
 switch(operator){
 case '+':
 correctAnswer= num1 + num2;
@@ -31,12 +32,24 @@ correctAnswer= num1-num2;
 break;
 case '*':
 correctAnswer= num1*num2;
+break;
 }
+let answer;
+do{
+	 answer = Number(readlineSync.question('Your answer: '));
+}while(Number.isNaN(answer));
+
+
 if (answer === correctAnswer){
-console.log(`Correct!\nCongratulations, ${name}`);
+console.log('Correct!');
+correctCount++;
 }
 else{
 console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
-}
+return;
+		}
+	}
+console.log(`Congratulations, ${name}!`);
+
 };
-export default calculate;
+
